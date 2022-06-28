ad3d03de77adb0b4090cba93890d3598f8d02ba8
LunaDesti28 <54392804+LunaticDes28@users.noreply.github.com>
2022-06-28 16:11:46 +0800

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,20 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://zh.m.wikipedia.org/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
 var mobileURLRe = /(?:zh.m.wikipedia.org|^)/;
 
 if (mobileURLRe.test(document.location.href)) {
   var target = document.location.href.replace("m.wikipedia", "wikipedia");
   window.location.replace(target)
 }
+})();
