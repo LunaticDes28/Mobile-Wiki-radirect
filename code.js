54b894a3b54d9f8a618bdc8e43efcd79d1abc8b8
LunaDesti28 <54392804+LunaticDes28@users.noreply.github.com>
2022-06-28 16:17:29 +0800

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  Mobile Wiki redirect
 // @author       You
 // @match        https://zh.m.wikipedia.org/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
